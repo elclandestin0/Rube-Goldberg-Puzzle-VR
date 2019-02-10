a5c8b63954aaf47b874a916910804c462b9dc0fc
Memo Khoury <majd.khoury93@gmail.com>
2019-02-09 20:53:29 -0500

    feat: log when ball collides with floor

---
+++ Assets/BallReset.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallReset : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("Ball has collided with " + col.gameObject.name);
+        
+        if (col.gameObject.name == "Floor")
+        {
+            Debug.Log("Reset the god damn ball dawg");
+        }
+    }
+
+}
