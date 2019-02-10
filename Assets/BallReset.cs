a971bd65fb3720c4934f4032847290e1ba934c74
Memo Khoury <majd.khoury93@gmail.com>
2019-02-09 22:08:32 -0500

    feat: reset ball by deactivating kinematic and lerping to pedestal

--- Assets/BallReset.cs
+++ Assets/BallReset.cs
@@ -4,23 +4,56 @@ using UnityEngine;
 
 public class BallReset : MonoBehaviour {
 
+    // Empty GameObject for ball to reset to if it hits the floor 
+    public GameObject Destination;
+
+    // Boolean to check if ball has collided with floor
+    public bool activatedLerp = false;
+
+    // Time to be added with deltaTime
+    float time = 0.0f;
+
+    // Time it takes to move
+    public float timeToMove = 2.0f;
+
+
+
     // Use this for initialization
     void Start() {
-
     }
 
     // Update is called once per frame
     void Update() {
-
+        if (activatedLerp == true)
+        {
+            Debug.Log("Collided with floor: " + activatedLerp);
+            ResetBall();
+        }
     }
 
     void OnCollisionEnter(Collision col)
     {
-        Debug.Log("Ball has collided with " + col.gameObject.name);
         
         if (col.gameObject.name == "Floor")
         {
-            Debug.Log("Reset the god damn ball dawg");
+            Debug.Log("Activated Lerp!");
+            activatedLerp = true;
+        }
+    }
+
+    void ResetBall()
+    {
+        gameObject.GetComponent<Rigidbody>().isKinematic = true;
+        Debug.Log("Resetting the ball ...");
+        time += Time.deltaTime;
+        transform.position = Vector3.Lerp(transform.position, Destination.transform.position, time / timeToMove);
+
+        if (transform.position == Destination.transform.position)
+        {
+            Debug.Log("Disabling lerp");
+            activatedLerp = false;
+            time = 0.0f;
+            gameObject.GetComponent<Rigidbody>().isKinematic = false;
         }
     }
 
