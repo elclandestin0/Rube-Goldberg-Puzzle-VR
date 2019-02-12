44bc73ac2e9ce8b93987f12134c6cd8a3c15df11
Memo Khoury <majd.khoury93@gmail.com>
2019-02-11 22:52:26 -0500

    feat: ball bounces from trampoline when colliding with it

---
+++ Assets/Bounce.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounce : MonoBehaviour
+{
+    // Amount of force applied to ball when bounce is activated
+    public float bounceForce = 5.0f;
+
+    // Bool to activate bounce
+    public bool bounce = false;
+
+    // Ball to add force to when bouncing
+    public Transform ball;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (bounce)
+        {
+			BallBounce();
+        }
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.name == "Ball")
+        {
+            bounce = true;
+        }
+    }
+
+    void BallBounce()
+    {
+        ball.GetComponent<Rigidbody>().AddForce(0, bounceForce, 0, ForceMode.Impulse);
+        bounce = false;
+    }
+}
