2b647db8d7a4f4d3e7b8136e19c4117e701bd20f
Memo Khoury <majd.khoury93@gmail.com>
2019-02-17 21:25:56 -0500

    feat: scene load new level when all stars are collected + goal collided

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -4,14 +4,24 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+
+
+    // List of star collectibles
+    public List<GameObject> collectibles;
+
+
     // Exit points for teleport blocks
     public GameObject ExitPointOne;
     public GameObject ExitPointTwo;
 
-    public GameObject playerController;
+    // Gameobject reference to the goal, in order to use the script's functions
+    public GameObject Goal;
 
     // Force of ball when teleporting
     public float force = 5.0f;
+
+    // count of the number of stars the ball collided with
+    static public int starsCollected;
 
     // Empty GameObject for ball to reset to if it hits the floor 
     public GameObject Destination;
@@ -19,19 +29,23 @@ public class Ball : MonoBehaviour
     // Boolean to check if ball has collided with floor
     public bool activatedLerp = false;
 
+    // Boolean to find exitPoints
+    public bool findExitPoints = false;
+
 
     // Time to be added with deltaTime
     float time = 0.0f;
 
     // Time it takes to move
     public float timeToMove = 3.0f;
-
-
-
     // Use this for initialization
     void Start()
     {
-
+        collectibles = new List<GameObject>();
+        foreach (GameObject collectible in GameObject.FindGameObjectsWithTag("Star"))
+        {
+            collectibles.Add(collectible);
+        }
     }
 
     // Update is called once per frame
@@ -43,6 +57,12 @@ public class Ball : MonoBehaviour
         }
         ExitPointOne = GameObject.FindGameObjectWithTag("ExitPointOne");
         ExitPointTwo = GameObject.FindGameObjectWithTag("ExitPointTwo");
+
+        if (starsCollected == collectibles.Count)
+        {
+            Goal.GetComponent<Goal>().starsHit = true;
+            starsCollected = 0;
+        }
     }
 
     void OnCollisionEnter(Collision col)
@@ -75,6 +95,7 @@ public class Ball : MonoBehaviour
         if (col.gameObject.tag == "Star")
         {
             col.gameObject.SetActive(false);
+            starsCollected++;
         }
     }
 
@@ -85,6 +106,16 @@ public class Ball : MonoBehaviour
         float lerpTime = time / timeToMove;
         transform.position = Vector3.Lerp(transform.position, Destination.transform.position, lerpTime);
 
+        // reactivate all collectibles from goal
+        ReactivateStars();
+
+        // reset starsCollected to zero
+        starsCollected = 0;
+
+        Goal.GetComponent<Goal>().starsHit = false;
+        Goal.GetComponent<Goal>().goalHit = false;
+
+
         if (lerpTime >= 1.0f)
         {
             activatedLerp = false;
@@ -92,5 +123,11 @@ public class Ball : MonoBehaviour
             gameObject.GetComponent<Rigidbody>().isKinematic = false;
         }
     }
-
+    public void ReactivateStars()
+    {
+        foreach (GameObject collectible in collectibles)
+        {
+            collectible.SetActive(true);
+        }
+    }
 }
---
+++ Assets/Goal.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve;
+using Valve.VR;
+public class Goal : MonoBehaviour
+{
+
+    public SteamVR_LoadLevel loadLevel;
+
+
+    public bool goalHit = false;
+    public bool starsHit = false;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (goalHit && starsHit)
+        {
+            Debug.Log("You won this level! Proceeding to the next level...");
+            Destroy(GameObject.FindGameObjectWithTag("Ball"));
+			loadLevel.Trigger();
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Ball")
+        {
+            goalHit = true;
+        }
+    }
+}
